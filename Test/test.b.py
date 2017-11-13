9840e7ef0a862ff7e68a44d4ea34c8e621b8787d
saruns <sarunnath57@gmail.com>
2017-11-13 20:47:51 +0530

    exercises

--- Test/test.b.py
+++ Test/test.b.py
@@ -1,17 +1,19 @@
 import sys
-def complex_num(x,y):
- m = complex(a.real, a.imag)
- n = complex(b.real, b.imag)
- p = m + n
- q = m * n
- print p.real,",",p.imag
- print q.real,",",q.imag
-class com(object):
- ""
-a = com()
-a.real = 1
-a.imag = 2
-b = com()
-b.real = 3
-b.imag = 4
-complex_num(a,b)
+class Complex:
+ def __init__(self,real,imag):
+  self.real=real
+  self.imag=imag
+ def __str__(self):
+  return str(self.real) + '+i' + str(self.imag)
+ def add(self, other):
+  c=Complex(self.real + other.real,self.imag + other.imag)
+  return c
+ def multi(self, other):
+  d=Complex(self.real*other.real - self.imag*other.imag,self.imag*other.real + self.real*other.imag)
+  return d
+a=Complex(1,2)
+b=Complex(3,4)
+c=a.add(b)
+d=a.multi(b)
+print c
+print d
